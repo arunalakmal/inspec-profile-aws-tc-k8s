dd04b240cc2f2a3575a00e5dc83eb509c6d397c5
Aruna Lakmal <arunarandunu@gmail.com>
2020-06-06 22:24:06 -0700

    Add files via upload

---
+++ controls/example.rb
@@ -0,0 +1,48 @@
+# control "Sample Contorl" do
+#   impact 0.9
+#   title "Sample Title"
+
+#   Replace this placeholder with test
+# end
+bucketname = attribute('bucketname')
+
+control "AWS S3" do
+  impact 0.7
+  title "Check AWS Backend S3 Buckets"
+
+  describe aws_s3_bucket(bucket_name: bucketname) do
+    it { should exist }
+    it { should_not be_public }
+  end
+end
+
+control "AWS EC2" do
+  impact 0.9
+  title "Check AWS EC2 Node Count"
+
+  describe aws_ec2_instances do
+    its('instance_ids.count') { should cmp 4 }
+  end
+end
+
+control "AWS EC2 AMI" do
+  impact 0.9
+  title "Check AWS EC2 Instance AMI"
+  aws_ec2_instances.instance_ids.each do |instance_id|
+    describe aws_ec2_instance(instance_id) do
+      it              { should_not have_roles }
+      its('key_name') { should cmp 'ironman' }
+      its('image_id') { should eq 'ami-0a887e401f7654935' }
+    end 
+  end
+end
+
+control "AWS Security Group" do
+  impact 0.9
+  title "Check Security Group Availability"
+
+  describe aws_security_group(group_name: 'tc_kubeadm_sg') do
+    it { should exist }
+  end
+  
+end
